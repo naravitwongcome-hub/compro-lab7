f000217d6f28e3a5784c5de0c3f26b5859bffbf2
GAME <naravitwongcome@gmail.com>
2025-12-12 22:41:35 +0700

    Lab7_3_complete.cpp

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,12 +2,25 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+    
+    if (x >= 'A' && x <= 'Z') {
+        
+        if (x == 'A') {
+            return 'Z';
+        } 
+        
+        else {
+            return x - 1;
+        }
+    } 
+    
+    else {
+        return '0';
+    }
 }
 
 int main(){
-	//Test Case
-	cout << before('A') << "\n";
+    cout << before('A') << "\n";
 	cout << before('B') << "\n";
 	cout << before('P') << "\n";
 	cout << before('T') << "\n";
@@ -15,5 +28,5 @@ int main(){
 	cout << before('a') << "\n";
 	cout << before('0') << "\n";
 	cout << before('c') << "\n";
-	return 0;
+    return 0;
 }
